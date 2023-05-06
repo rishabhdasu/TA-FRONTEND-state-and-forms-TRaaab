7848fd3a848db5d328e8673641af62e874d079c7
Rishabh <rishabhdasu@gmail.com>
2023-05-07 02:28:14 +0530

    Update

--- block-BRaabr/code/form/src/index.js
+++ block-BRaabr/code/form/src/index.js
@@ -2,29 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-const { defaults } = require("jest-config");
-
-/** @type {import('jest').Config} */
-const config = {
-  moduleFileExtensions: [...defaults.moduleFileExtensions, "mts", "cts"],
-};
-
-/** @type {import('jest').Config} */
-const confi = {
-  moduleNameMapper: {
-    "^image![a-zA-Z0-9$_-]+$": "GlobalImageStub",
-    "^[./a-zA-Z0-9$_-]+\\.png$": "<rootDir>/RelativeImageStub.js",
-    "module_name_(.*)": "<rootDir>/substituted_module_$1.js",
-    "assets/(.*)": [
-      "<rootDir>/images/$1",
-      "<rootDir>/photos/$1",
-      "<rootDir>/recipes/$1",
-    ],
-  },
-};
-module.exports = config;
-module.exports = confi;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
