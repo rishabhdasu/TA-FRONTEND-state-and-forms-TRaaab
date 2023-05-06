5707403c49a7996aa66a3107a87d367da4df5815
Rishabh <rishabhdasu@gmail.com>
2023-05-07 01:44:11 +0530

    moduleNameMapper

--- block-BRaabr/code/form/src/index.js
+++ block-BRaabr/code/form/src/index.js
@@ -13,4 +13,18 @@ const config = {
   moduleFileExtensions: [...defaults.moduleFileExtensions, "mts", "cts"],
 };
 
+/** @type {import('jest').Config} */
+const confi = {
+  moduleNameMapper: {
+    "^image![a-zA-Z0-9$_-]+$": "GlobalImageStub",
+    "^[./a-zA-Z0-9$_-]+\\.png$": "<rootDir>/RelativeImageStub.js",
+    "module_name_(.*)": "<rootDir>/substituted_module_$1.js",
+    "assets/(.*)": [
+      "<rootDir>/images/$1",
+      "<rootDir>/photos/$1",
+      "<rootDir>/recipes/$1",
+    ],
+  },
+};
 module.exports = config;
+module.exports = confi;
