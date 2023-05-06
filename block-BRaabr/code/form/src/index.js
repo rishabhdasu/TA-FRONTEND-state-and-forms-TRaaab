b4c59c13584324bd19776ef1461f5a80fb54a80c
Rishabh <rishabhdasu@gmail.com>
2023-05-07 01:39:38 +0530

    transformIgnorePatterns

--- block-BRaabr/code/form/src/index.js
+++ block-BRaabr/code/form/src/index.js
@@ -5,3 +5,12 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+const { defaults } = require("jest-config");
+
+/** @type {import('jest').Config} */
+const config = {
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "mts", "cts"],
+};
+
+module.exports = config;
