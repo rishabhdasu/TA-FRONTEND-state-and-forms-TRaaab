24f3ecddf510c94fe5615b55a0d98b219351ca24
Rishabh <rishabhdasu@gmail.com>
2023-05-07 02:35:49 +0530

    Test2

--- block-BRaabr/code/form/src/App.test.js
+++ block-BRaabr/code/form/src/App.test.js
@@ -6,3 +6,10 @@
 //   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
+module.exports = {
+  "@disabled": true, // This will prevent the test module from running.
+
+  "sample test": function (browser) {
+    // test code
+  },
+};
