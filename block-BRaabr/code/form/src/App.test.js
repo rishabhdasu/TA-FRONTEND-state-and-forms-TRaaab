ffa2a8c29af67cd9814b52b06e6e5e24d74f811e
Rishabh <rishabhdasu@gmail.com>
2023-05-07 13:26:05 +0530

    test5

--- block-BRaabr/code/form/src/App.test.js
+++ block-BRaabr/code/form/src/App.test.js
@@ -3,9 +3,9 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(
-    <>
+    <div>
       <App />
-    </>
+    </div>
   );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
